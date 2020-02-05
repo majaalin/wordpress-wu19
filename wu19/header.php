7dba3ad7e864e3a53b40f9730b0c900424d64420
majaalin <maja_alin@hotmail.com>
2020-02-05 17:33:39 +0100

    Updated header and footer

--- wu19/header.php
+++ wu19/header.php
@@ -1,4 +1,3 @@
-  
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
@@ -6,7 +5,19 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
   <?php wp_head(); ?>
 </head>
 <body>
+<div class="container">
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <?php $pages = get_pages();?>
+        <?php foreach ($pages as $page => $data): 
+            $title = $data->post_title;
+            $url = $data->guid;
+            ?>
+            <a href="<?php echo $url;?>" class="navbar-brand"><?php echo $title;?></a>
+        <?php endforeach; ?>
+    </nav>
